1ab801a134eb7a417161127399cc52ee50e7ffec
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-02-05 00:11:37 +0530

    Initial Commit

---
+++ SwiftuiSocialMediaProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftuiSocialMediaProject
+//
+//  Created by Bhavik Bhatt on 05/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftuiSocialMediaProject/SwiftuiSocialMediaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiSocialMediaProjectApp.swift
+//  SwiftuiSocialMediaProject
+//
+//  Created by Bhavik Bhatt on 05/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiSocialMediaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
